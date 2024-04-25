73cecd3c5bbcb2a68fd484e842c85d5e33c9718a
IrtizaJavaid <139428612+IrtizaJavaid@users.noreply.github.com>
2024-04-25 15:43:20 +0530

    added README.md and completed navbar

---
+++ index.js
@@ -0,0 +1,10 @@
+// var state = {
+//     Tasklist : [
+//         {
+//             imageurl : "",
+//             tasktitle : "",
+//             taskType : "",
+//             taskDesciption : ""
+//         }
+//     ]
+// }
